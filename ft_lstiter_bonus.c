b13294e109420e5d014f392e4544a1f1c02ae142
Basma <essabribassma062@gmail.com>
2024-11-13 11:38:01 +0100

    Update ft_lstiter_bonus.c

--- ft_lstiter_bonus.c
+++ ft_lstiter_bonus.c
@@ -33,25 +33,3 @@ void	ft_lstiter(t_list *lst, void (*f)(void *))
 		lst = lst->next;
 	}
 }
-/*int main()
-{
-	int *n1 = malloc(sizeof(int));
-	*n1 = 1;
-	t_list *node1 =ft_lstnew(n1);
-	int *n2 = malloc(sizeof(int));
-	*n2 = 2;
-	t_list *node2 =ft_lstnew(n2);
-	int *n3 = malloc(sizeof(int));
-	*n3 = 5;
-	t_list *node3 =ft_lstnew(n3);
-	ft_lstadd_back(&node1,node2);
-	ft_lstadd_back(&node1,node3);
-	ft_lstiter(node1, ddouble);
-	t_list *c = node1;
-	while (c != NULL)
-	{
-	printf("%d\n",*(int *)c->content);
-	c = c->next;
-	}
-	//ft_lstiter(lst, print_function);
-}*/
